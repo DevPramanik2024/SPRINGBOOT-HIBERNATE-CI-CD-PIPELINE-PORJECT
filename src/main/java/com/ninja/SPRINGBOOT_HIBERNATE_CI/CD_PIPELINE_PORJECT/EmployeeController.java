63a1779fdeff8f46078345e4c8a255fd2e9ae554
Devkumar87 <kdev80170@gmail.com>
2025-05-16 09:37:13 +0530

    second commit

--- src/main/java/com/ninja/SPRINGBOOT_HIBERNATE_CI/CD_PIPELINE_PORJECT/EmployeeController.java
+++ src/main/java/com/ninja/SPRINGBOOT_HIBERNATE_CI/CD_PIPELINE_PORJECT/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.ninja.SPRINGBOOT_HIBERNATE_CI.CD_PIPELINE_PORJECT;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +27,19 @@ public class EmployeeController
 	{
 		erepo.save(employee);
 		return "Data save in tables ";
+	}
+	
+	@GetMapping("/by/{name}")
+	public List<Employee> byname(@PathVariable String name)
+	{
+		
+		return erepo.findByName(name);
+	}
+	
+	@GetMapping("/all")
+	public List<Employee> alldata()
+	{
+		return erepo.findAll();
 	}
 	
 }
--- src/main/java/com/ninja/SPRINGBOOT_HIBERNATE_CI/CD_PIPELINE_PORJECT/EmployeeRepository.java
+++ src/main/java/com/ninja/SPRINGBOOT_HIBERNATE_CI/CD_PIPELINE_PORJECT/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package com.ninja.SPRINGBOOT_HIBERNATE_CI.CD_PIPELINE_PORJECT;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-	
+	List<Employee> findByName(String name);
 
 }
